d848e26e6f08f28c03fe1771a145a1e95b69c840
chLi <565353780@qq.com>
2026-04-20 15:41:54 +0800

    create setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,12 @@
+pip install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu124
+
+pip install matplotlib onnxruntime rembg numpy einops \
+  pandas pillow huggingface_hub scipy torchmetrics tqdm \
+  transformers scikit-learn opencv-python timm
+
+pip install pydantic==2.10.6
+pip install gradio==5.9.0
+pip install accelerate==1.8.1
+pip install lightning==2.2
+pip install bpy==4.2
